45ea850b26ed46faabd86cf6a0f71fdb9fbdfd0f
Riku Brandt <brandtriku@gmail.com>
2021-11-22 12:47:30 +0200

    Teht 1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,30 @@
 from player import Player
+import requests
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['team'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['nationality']
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, assists, goals, nationality):
         self.name = name
+        self.team = team
+        self.assists = assists
+        self.goals = goals
+        self.nationality = nationality
     
     def __str__(self):
-        return self.name
+        return "%s team %s goals %d assists %d" % (self.name, self.team, self.goals, self.assists)

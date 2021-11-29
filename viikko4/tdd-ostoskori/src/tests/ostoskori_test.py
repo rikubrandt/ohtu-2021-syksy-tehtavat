6c0716dca5556b48d9e432318c091ab2767d02ca
Riku Brandt <brandtriku@gmail.com>
2021-11-29 20:45:27 +0200

    tdd 12

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -78,3 +78,11 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
+
+    def test_sama_tuote_ostoksien_lukumaara(self):
+        maito = Tuote("Maito", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        ostos = ostokset[0]
+        self.assertEqual(ostos.lukumaara(), 2)
